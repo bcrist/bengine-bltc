330b3751981d7de1701870a1738da6cb6d220cd1
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:57:52 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,18 +1,13 @@
-tool { name = 'bltc',
-   projects = {
-      app {
-         icon = 'icon/bengine-warm.ico',
-         src = {
-            'src/*.cpp'
-         },
-         libs = {
-            'core',
-            'core-id-with-names',
-            'util',
-            'cli',
-            'ctable',
-            'blt'
-         }
+tool 'bltc' {
+   app {
+      icon 'icon/bengine-warm.ico',
+      link_project {
+         'core',
+         'core-id-with-names',
+         'util',
+         'cli',
+         'ctable',
+         'blt'
       }
    }
 }
